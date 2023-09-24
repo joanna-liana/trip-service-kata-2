45b7addd213e268c65abe1309ac6cd5bbe620f59
Joanna Liana <joanna_liana@outlook.com>
2023-09-24 09:13:32 +0200

    Refactor: remove trips from User as they're handled by DAO

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,19 +1,8 @@
-import Trip from "../trip/Trip";
-
 export default class User {
-    private trips: Trip[] = [];
     private friends: User[] = [];
 
     public addFriend(user: User): void {
         this.friends.push(user);
-    }
-
-    public addTrip(trip: Trip): void {
-        this.trips.push(trip);
-    }
-
-    public getTrips(): Trip[] {
-        return this.trips;
     }
 
     public isFriendsWith(anotherUser: User) {
