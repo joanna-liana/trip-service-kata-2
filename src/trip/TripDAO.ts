ac438b11bc7bc13a0dfd95f02a5acb02ea846fc7
Joanna Liana <joanna_liana@outlook.com>
2023-09-24 08:05:48 +0200

    Migrate from tslint to eslint

--- src/trip/TripDAO.ts
+++ src/trip/TripDAO.ts
@@ -3,8 +3,9 @@ import User from "../user/User";
 import Trip from "./Trip";
 
 export default class TripDAO {
-    public static findTripsByUser(user: User): Trip[] {
+    public static findTripsByUser(_user: User): Trip[] {
         throw new CollaboratorCallException(
-            "TripDAO should not be invoked on an unit test.");
+            "TripDAO should not be invoked on an unit test."
+        );
     }
 }
