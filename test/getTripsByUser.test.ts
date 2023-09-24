6088701e105a8460146455de8142c59655a1ab00
Joanna Liana <joanna_liana@outlook.com>
2023-09-24 08:24:00 +0200

    Add a characterisation for the next shallowest branch

--- test/getTripsByUser.test.ts
+++ test/getTripsByUser.test.ts
@@ -5,6 +5,22 @@ import TripService from "../src/trip/TripService";
 import User from "../src/user/User";
 
 describe("Get trips by user use case", () => {
+    describe("given a logged in user", () => {
+        it("returns an empty trip list", () => {
+            // given
+            const user = new User();
+            const sessionWithUser = { getLoggedUser: () => user };
+            const sut = new TripService(sessionWithUser);
+
+            // when
+            const tripList = sut.getTripsByUser(user);
+
+            // then
+            expect(tripList).toStrictEqual([]);
+        });
+    });
+
+
     it("does not allow to use the system without logging in", () => {
         // given
         const user = new User();
