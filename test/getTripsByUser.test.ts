ca39cf831d4eca8ad24599d99264c07ec41c22ba
Joanna Liana <joanna_liana@outlook.com>
2023-09-24 08:32:27 +0200

    Cover another branch (failing)

--- test/getTripsByUser.test.ts
+++ test/getTripsByUser.test.ts
@@ -5,7 +5,22 @@ import TripService from "../src/trip/TripService";
 import User from "../src/user/User";
 
 describe("Get trips by user use case", () => {
-    describe("given a logged in user", () => {
+    describe("given a logged in requester", () => {
+        it("returns a trip list if the requester is a friend of the queried user", () => {
+            // given
+            const requester = new User();
+            const friend = new User();
+            friend.addFriend(requester);
+            const sessionWithUser = { getLoggedUser: () => requester };
+            const sut = new TripService(sessionWithUser);
+
+            // when
+            const tripList = sut.getTripsByUser(friend);
+
+            // then
+            expect(tripList).toStrictEqual([]);
+        });
+
         it("returns an empty result if the user tries to list their own trips", () => {
             // given
             const requester = new User();
